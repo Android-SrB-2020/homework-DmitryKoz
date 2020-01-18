ad729e1049a7845aa1680bc5d7b83651339c32cf
Kozyriev Dmytro <kozyriev@ukr.net>
2020-01-18 16:27:38 -0400

    Homework 1_2 added reset button and provided answers to questions in form of comments on the top of MainActivity.kt file

--- 1FirstApps/LessonOne/Lesson_1_2_DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
+++ 1FirstApps/LessonOne/Lesson_1_2_DiceRoller/app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,3 +1,11 @@
+//Question 1
+//Which method on an Activity inflates the app's layout and makes its views available as objects?
+//setClickListener()
+//
+//Question 2
+//Which view attribute do you use to set the width of a view so that it adjusts to fit the content?
+//android:layout_width="wrap_content"
+
 package com.example.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
@@ -18,6 +26,9 @@ class MainActivity : AppCompatActivity() {
 
         val countButton: Button = findViewById(R.id.countUp_button)
         countButton.setOnClickListener {addCount()}
+
+        val resetButton: Button = findViewById(R.id.reset_button)
+        resetButton.setOnClickListener {reset()}
     }
 
     private fun rollDice(){
@@ -29,21 +40,27 @@ class MainActivity : AppCompatActivity() {
       //  Toast.makeText(this, "button clicked", Toast.LENGTH_SHORT).show()
     }
 
-    private fun addCount(){
+    private fun addCount() {
         val resultText: TextView = findViewById(R.id.result_text)
 
-        if (resultText.text != "Hello World!"){
+        if (resultText.text != "Hello World!") {
 
             var resultInt = resultText.text.toString().toInt()
 
-            if (resultInt <6 ){
+            if (resultInt < 6) {
                 resultInt++
-                resultText.text=resultInt.toString()
+                resultText.text = resultInt.toString()
             }
-        }
-        else{
-            resultText.text="1"
+        } else {
+            resultText.text = "1"
         }
     }
 
+    private fun reset(){
+
+        val resultText: TextView = findViewById(R.id.result_text)
+
+        var resultInt = 0
+        resultText.text=resultInt.toString()
+    }
 }
