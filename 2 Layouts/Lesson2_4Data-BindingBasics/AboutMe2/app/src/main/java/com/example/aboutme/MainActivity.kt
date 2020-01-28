28f8882348e894f7b7458e5b293cebcb18297498
Kozyriev Dmytro <kozyriev@ukr.net>
2020-01-27 22:40:18 -0400

    submission of homework 2.4. data binding

---
+++ 2 Layouts/Lesson2_4Data-BindingBasics/AboutMe2/app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -0,0 +1,39 @@
+//
+package com.example.aboutme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Kozyriev Dmitry")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+
+    }
+
+
+    private fun addNickname(view: View) {
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+    }
+    }
