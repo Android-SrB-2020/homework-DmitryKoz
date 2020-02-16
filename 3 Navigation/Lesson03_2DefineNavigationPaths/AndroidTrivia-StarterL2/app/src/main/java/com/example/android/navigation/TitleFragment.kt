718909ae4dbb1d738c8fdba6b5b1481033430095
Kozyriev Dmytro <kozyriev@ukr.net>
2020-02-16 00:12:11 -0400

    fixed version of 3.1. homework exercise

--- 3 Navigation/Lesson03_2DefineNavigationPaths/AndroidTrivia-StarterL2/app/src/main/java/com/example/android/navigation/TitleFragment.kt
+++ 3 Navigation/Lesson03_2DefineNavigationPaths/AndroidTrivia-StarterL2/app/src/main/java/com/example/android/navigation/TitleFragment.kt
@@ -17,12 +17,12 @@ class TitleFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-
         val binding = DataBindingUtil.inflate<FragmentTitleBinding>(inflater,
                 R.layout.fragment_title,container,false)
         return binding.root
+    }
 
     }
 
 
-}
+
